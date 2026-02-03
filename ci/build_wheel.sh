a3cd02026420916a2dff8fd1318b3cc894432e6c
James Lamb <jaylamb20@gmail.com>
2026-02-03 13:01:33 -0600

    remove pip.conf migration code in CI scripts (#66)
    Follow-up to changes for https://github.com/rapidsai/build-planning/issues/241
    
    * removes `rapids-init-pip` feature flags now that those have become the default behavior (https://github.com/rapidsai/gha-tools/pull/242)
    
    Authors:
      - James Lamb (https://github.com/jameslamb)
    
    Approvers:
      - Kyle Edwards (https://github.com/KyleFromNVIDIA)
    
    URL: https://github.com/rapidsai/rapids-logger/pull/66

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -10,8 +10,6 @@ dist_dir="${package_dir}/dist"
 final_dir="${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
 
 source rapids-configure-sccache
-RAPIDS_INIT_PIP_REMOVE_NVIDIA_INDEX="true"
-export RAPIDS_INIT_PIP_REMOVE_NVIDIA_INDEX
 source rapids-init-pip
 
 rapids-logger "Building '${package_name}' wheel"
